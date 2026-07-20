4e8cc246da260b8b3e0e53bbd0a38ff35d46e9ab
Alexander Lazarov <alexander.lazaroff@gmail.com>
2026-07-20 21:15:49 +0300

    Init project

---
+++ Lost in Integration/Lost in Integration/Lost_in_IntegrationApp.swift
@@ -0,0 +1,32 @@
+//
+//  Lost_in_IntegrationApp.swift
+//  Lost in Integration
+//
+//  Created by Alexander Lazarov on 20.07.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Lost_in_IntegrationApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
